996e4496bac7473bd175b39daaeead58e300016f
shinojeattath <109549717+shinojeattath@users.noreply.github.com>
2023-11-21 15:19:50 +0530

    update views
    delted http response

--- hod/views.py
+++ hod/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def hod_login(request):
     return render(request, 'hod/login.html')
 
-def test(request):
-    return HttpResponse("hello")
