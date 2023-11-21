05fe9a6d82b93871e32e666b303cfee110f1bcac
shinojeattath <109549717+shinojeattath@users.noreply.github.com>
2023-11-21 16:28:38 +0530

    LOGIN
    created login function signup function profile page

--- staff/urls.py
+++ staff/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path("", views.homepage,name='homepage'),
     path("login/",views.user_login, name='user_login'),
+    path("signup/",views.signup,name='signup'),
+    path("profile/",views.profile,name='profile'),
     
 ]
--- staff/views.py
+++ staff/views.py
@@ -1,8 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#from .models import Details, Leave_Application, Approved_Leave_Application
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.utils import timezone
 
 # Create your views here.
 def homepage(request):
     pass
 
 def user_login(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        pass1 = request.POST['password']
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            request.session['username'] = username
+            if user.groups.filter(name='STAFF').exists():
+                return redirect('profile')
+            else:
+                messages.error(request, "Invalid username or Password")
+                return render(request, 'staff/login.html')
+    
+           
+        else:
+            messages.error(request, "Invalid username or Password")
+            return render(request, 'staff/login.html')
     return render(request, 'staff/login.html')
+
+def profile(request):
+    return render(request, 'staff/profile.html')
+def signout(request):
+    pass
+def leave_application(request):
+    pass
+def show_leave_application(request):
+    pass
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        #password1 = request.POST['password1']
+
+        
+        user = User.objects.create_user(username,email,password)
+        user.save()
+        return redirect('user_login')  
+    return render(request, 'staff/signup.html')
