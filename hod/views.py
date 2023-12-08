88be626fef8653af497c889a76536f9ad1ee7047
shinojeattath <109549717+shinojeattath@users.noreply.github.com>
2023-12-08 10:46:21 +0530

    login funstion for hod

--- hod/views.py
+++ hod/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.utils import timezone
+from staff.models import Staff_Details, Leave_Application, Status_Leave_Application
 
 
 # Create your views here.
 def hod_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        pass1 = request.POST['password']
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            request.session['username'] = username
+            if user.groups.filter(name='HOD').exists():
+                return redirect('leave_requests')
+            else:
+                messages.error(request, "Invalid username or Password")
+                return render(request, 'hod/login.html')
+        else:
+            messages.error(request, "Invalid username or Password")
     return render(request, 'hod/login.html')
 
