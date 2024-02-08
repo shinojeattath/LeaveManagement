a9cf23aa482c8561b9def7daff943bbb4bcd4ec0
shinojeattath <109549717+shinojeattath@users.noreply.github.com>
2024-02-08 09:59:33 +0530

    LEAVE REJECTED

--- staff/admin.py
+++ staff/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Staff_Details, Status_Leave_Application, Leave_Application
+from .models import Staff_Details, Status_Leave_Application, Leave_Application, AlternateArrangements
 # Register your models here.
 
 admin.site.register(Staff_Details)
 admin.site.register(Status_Leave_Application)
 admin.site.register(Leave_Application)
+admin.site.register(AlternateArrangements)
--- staff/urls.py
+++ staff/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path("signup/",views.signup,name='signup'),
     path("profile/",views.profile,name='profile'),
     path("signout/",views.signout, name = "signout"),
-    path("new_leave_application/",views.new_leave_application, name='new_leave_application'),
+    path("new_leave_application/",views.new_leave_application,name='new_leave_application'),
     path("show_leave_application/",views.show_leave_application,name='show_leave_application'),
+    path("new_leave_application_2",views.new_leave_application_2,name='new_leave_application_2'),
     
     
 ]
