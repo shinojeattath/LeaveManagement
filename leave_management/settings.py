ef6cfdcbcc31f83169f8d9dbaf14a0e94db918ca
shinojeattath <109549717+shinojeattath@users.noreply.github.com>
2024-02-11 16:12:39 +0530

    HR FUNCTIONS

--- leave_management/settings.py
+++ leave_management/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'staff',
     'hod',
     'bootstrap5',
+    'hr',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = "leave_management.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS": [os.path.join(BASE_DIR, 'templates'),],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
