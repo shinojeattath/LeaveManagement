b519d1a467025d57cd92a8ac79913a5156a6f86e
DIYA EL FADHIL <133634488+diya-el-fadhil@users.noreply.github.com>
2024-02-14 12:31:07 +0530

    new leave 3

--- staff/urls.py
+++ staff/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("new_leave_application/",views.new_leave_application,name='new_leave_application'),
     path("show_leave_application/",views.show_leave_application,name='show_leave_application'),
     path("new_leave_application_2",views.new_leave_application_2,name='new_leave_application_2'),
+    path("new_leave_application_3",views.new_leave_application_3,name='new_leave_application_3'),
     path("upload",views.upload,name='upload')
     
     
