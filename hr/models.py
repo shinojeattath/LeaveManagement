e18ae5f755317d157381665b0353e35cb9e1d872
shinojeattath <109549717+shinojeattath@users.noreply.github.com>
2024-02-16 11:39:36 +0530

    staff certificates

--- hr/models.py
+++ hr/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from staff.models import Staff_Details
 # Create your models here.
+class Leave_Application_hr(models.Model):
+
+    employee_id = models.ForeignKey(Staff_Details,on_delete=models.CASCADE, related_name="emp_id4")
+    name = models.CharField(max_length=20)
+    department = models.CharField(max_length=5)
+    nature_of_leave = models.CharField(max_length=20)
+    no_of_days = models.IntegerField(null = True)
+    leave_from = models.DateField(null = False)
+    reason = models.CharField(max_length=200)
+    status_of_request = models.CharField(max_length=10, default="PENDING")
+    time_of_request = models.DateTimeField(null=True)
+    alt_linways_assigned = models.CharField(max_length=5, null=True)
+    submitted = models.BooleanField(default=False)
+    certificate = models.FileField(upload_to='media/')
